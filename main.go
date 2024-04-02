9945f9af1f940f3d14d9510395ddbc9e1687b582
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2024-04-03 06:17:17 +0900

    [GO-16] feat: 환경변수 사용하여 코드 수정

--- main.go
+++ main.go
@@ -9,25 +9,28 @@ import (
 	"os"
 
 	"golang.org/x/sync/errgroup"
+
+	"github.com/new-pow/go_todo_app/config"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Fatalf("failed to terminate server: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New() // config 생성
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port)) // 포트 설정
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("server listening at %s", url)
 	s := &http.Server{
 		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
