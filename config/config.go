7c964b6783d09cc129e27a485f9b547b00791f4a
Lee Saehim <103120173+new-pow@users.noreply.github.com>
2024-04-03 02:18:02 +0900

    [GO-16] docs: 코드 설명 추가

--- config/config.go
+++ config/config.go
@@ -10,8 +10,10 @@ type Config struct {
 }
 
 func New() (*Config, error) {
-	cfg := &Config{}
-	if err := env.Parse(cfg); err != nil { // 환경 변수 parse
+	cfg := &Config{} // 구조체 포인터 생성
+	// 환경 변수 parse
+	// Parse 함수는 'env' 태그가 포함된 구조체를 구문 분석하고 환경 변수에서 해당 값을 로드합니다.
+	if err := env.Parse(cfg); err != nil {
 		return nil, err
 	}
 	return cfg, nil
